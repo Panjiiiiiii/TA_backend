0ec45bc00063440be92675f7491a1280c7b8f74b
Panjiiiiiii <110644169+Panjiiiiiii@users.noreply.github.com>
2024-03-07 08:39:36 +0700

    revisi 3

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.pemesanan, {foreignKey: 'id_user', as: 'pememsanan'});
+      this.hasMany(models.pemesanan, {foreignKey: 'id_user', as: 'pememsanan'});
     }
   }
   user.init({
